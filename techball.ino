b43bcb6df2aaefb5d7cd74e5569e000a044c3001
Cooper Brislain <cbrislain@gmail.com>
2018-03-03 20:13:15 -0800

    first

---
+++ techball.ino
@@ -0,0 +1,67 @@
+#include "FastLED.h"
+
+#define NUM_LEDS 6
+
+#define DATA_PIN 7
+#define BRIGHTNESS_PIN 0
+#define SPEED_PIN 1
+#define MIN_BRIGHTNESS 32
+#define MAX_BRIGHTNESS 255
+
+CRGB leds[NUM_LEDS];
+
+int index;
+int count;
+int cur_color;
+int brightness;
+
+void fade(){
+    for(int i=0; i<NUM_LEDS; i++){ 
+         leds[i].fadeToBlackBy( 20 );
+    }
+}
+
+void blackout(){
+    for(int i=0; i<NUM_LEDS; i++){ 
+         leds[i]=CRGB::Black;
+    }
+}
+
+void fade_out_by(int b){
+    if(brightness > 0) {
+        brightness -= b;
+        FastLED.setBrightness(brightness);
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    for (int i=0; i<NUM_LEDS; i++) {
+        leds[i] = CRGB::Black;  
+    }
+    FastLED.show();
+    count=0;
+    randomSeed(analogRead(A0)); 
+}
+
+void march() {
+    leds[count%NUM_LEDS] = CRGB::White;
+}
+
+void warm() {
+    int which = random(NUM_LEDS);
+    leds[which] += CHSV(255,255,255);
+}
+
+void loop() {
+    int val = analogRead(BRIGHTNESS_PIN);
+    //Serial.println(val);
+    blackout();
+    march();
+
+    //fade();
+    FastLED.show();
+    count++;
+    delay(1024-val);
+}

ffccbef389021acd0859a1866f51828c21ba565f
Cooper Brislain <cbrislain@gmail.com>
2018-03-28 10:19:51 -0700

    better strobing

--- techball.ino
+++ techball.ino
@@ -1,12 +1,13 @@
 #include "FastLED.h"
 
-#define NUM_LEDS 12
+#define NUM_LEDS 10
 
 #define DATA_PIN 7
 #define BRIGHTNESS_PIN 0
 #define SPEED_PIN 1
 #define MIN_BRIGHTNESS 32
 #define MAX_BRIGHTNESS 255
+#define PERIOD 150
 
 CRGB leds[NUM_LEDS];
 
@@ -14,11 +15,12 @@ int index;
 int index2;
 int count;
 int color;
+int color2;
 int brightness;
 
 void fade(){
     for(int i=0; i<NUM_LEDS; i++){
-         leds[i].fadeToBlackBy( 20 );
+         leds[i].fadeToBlackBy( 1 );
     }
 }
 
@@ -39,20 +41,25 @@ void setup() {
 }
 
 void loop() {
-    flare_compliments();
     fade();
+    arc_compliments();
     FastLED.show();
     count++;
-    delay(90);
+    delay(10);
 }
 
-void flare_compliments() {
-    if(count%20==0) {
-        index = random(NUM_LEDS);
+void arc_compliments() {
+    if(count%PERIOD==0) {
+        index = index2;
         index2 = random(NUM_LEDS);
         if (index2 == index) index2 = (index2+NUM_LEDS/2)%NUM_LEDS;
-        color = random8();
+        color = color2;
+        color2 = (color2+10)%255;
     }
-    leds[index]+=CHSV(color,255,55);
-    leds[index2]+=CHSV((color+128)%255,255,55);
+    leds[index]+=CHSV(color,255,25);
+    if(count%7 < 2) {
+        leds[index2]+=CHSV(color2,(count%PERIOD)*(255/PERIOD),(125+random8()/6)-count%(PERIOD/7));
+    } else {
+        leds[index2].fadeToBlackBy( 45 );
+    }
 }
